5b6fc668ab66afa8f16b83153beeb5a9969f246a
nicolasvanbaren <nicolasvanbaren@me.com>
2018-09-20 11:29:28 +0200

    minor changes

--- Rscript_TCGA_table_generation_combined.R
+++ Rscript_TCGA_table_generation_combined.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(stringr)
 
-setwd("/media/nicolas/ce71f585-e4d4-43a3-adea-f042dd3525082/TCGA_rnaseq")                              #set working directory as appropriate (it must contain the source files) 
+setwd("/Users/vanbaren/TCGA_rnaseq")                              #set working directory as appropriate (it must contain the source files) 
 
 #Step1: generate a data frame (tibble_expr) with genes (identified by Ensembl gene identifier) as rows and samples (identified by their GDC manifest file identifier) as columns.
-file_names <- list.files(path = "TCGA_rnaseq_FPKM-UQ", pattern = ".FPKM-UQ.txt$", all.files = FALSE, full.names = TRUE)     #generate a vector listing the names of the files
+file_names <- list.files(path = "TCGA_rnaseq_counts", pattern = ".counts$", all.files = FALSE, full.names = TRUE)     #generate a vector listing the names of the files
 gene_list <- read_tsv(file_names[1], col_names = FALSE)                                                                     #create a new tibble, copy the content of the first data file
 colnames(gene_list) <- c("ensembl_gene_id", "x")                                                                            #name the 1st column with the gene identifier, 2nd column x, which will be deleted soon)
 gene_list <- dplyr::select(gene_list, ensembl_gene_id) %>%                                                                  #keep only 1st column; add dplyr:: to the select function, which has another action in base R
@@ -14,8 +14,8 @@ tibble_expr <- gene_list                                                        
 counter <- 0                                                                                                #the counter that will allow to follow the progress of the loop is set to zero
 for (sample_name in file_names) {                                                                           #start of the loop: sample files are processed 1 by 1
   tibble_temp <- read_tsv(sample_name, col_names = FALSE)                                                   #import sample file
-  buffer_name <- str_replace(sample_name, "TCGA_rnaseq_FPKM-UQ/", "")                                       #generate a sample name without the prefix
-  buffer_name <- str_replace(buffer_name, ".FPKM-UQ.txt", "")                                               #remove the suffix from this sample name
+  buffer_name <- str_replace(sample_name, "TCGA_rnaseq_counts/", "")                                       #generate a sample name without the prefix
+  buffer_name <- str_replace(buffer_name, ".counts", "")                                               #remove the suffix from this sample name
   colnames(tibble_temp) <- c("ensembl_gene_id", buffer_name)                                                #rename the columns
   tibble_expr <- full_join(tibble_expr, tibble_temp, by = "ensembl_gene_id")                                #join this table to the growing table with the gene names and the sample values already added
   counter <- counter + 1                                                                                    #increase counter value by 1
@@ -24,7 +24,7 @@ for (sample_name in file_names) {                                               
 tibble_expr$ensembl_gene_id <- str_sub(tibble_expr$ensembl_gene_id,1,15)                                    #remove suffix (version) from the gene identifiers
 tibble_expr <- arrange(tibble_expr, ensembl_gene_id)                                                        #sort by gene ID
 
-#write.table(tibble_expr, file = "tibble_expr.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)      #if necessary (to avoid repeated lengthy runs) save data as new table
+write.table(tibble_expr, file = "tibble_expr_counts.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)      #if necessary (to avoid repeated lengthy runs) save data as new table
 
 #tibble_expr <- read_tsv("tibble_expr.txt")                                                                              #load table data (start here if previous step was done and data saved)
 
@@ -50,7 +50,7 @@ tibble_hgnc <- left_join(mart_annot, tibble_expr, by = "ensembl_gene_id") %>%   
 tibble_hgnc <- gather(tibble_hgnc, key = manifest_file_id, value = value, 2:ncol(tibble_hgnc))        #transpose the data frame (1st step: transform in tidy data)
 tibble_hgnc <- spread(tibble_hgnc, key = hgnc_symbol, value = 'value')                                #transpose the data frame (2nd step: untidy with gene symbols as column headers)
 
-manifest <- read.table("Downloaded_from_GDC/gdc_manifest.2018-09-06_FPKM-UQ.txt", header = TRUE)    #import manifest file obtained from GDC website
+manifest <- read.table("Downloaded_from_GDC/gdc_manifest.2018-09-06_counts.txt", header = TRUE)    #import manifest file obtained from GDC website
 file_uuids <- manifest$id                                                                   #retrieve the file identifiers
 #the following loop was copied from a website (https://www.biostars.org/p/306400/), I do not master its instructions, but it works
 library(GenomicDataCommons)
@@ -76,9 +76,10 @@ corresp_sampleUUID_barcode <- corresp_sampleUUID_barcode %>%
 
 corresp_sampleUUID_barcode$tissue_type <- recode_factor(corresp_sampleUUID_barcode$tissue_type, "01" = "primary_tumor","02" = "primary_tumor","03" = "primary_tumor","05" = "primary_tumor","06"="metastasis","07"="metastasis","11"="adjacent_normal")
 
-manifest <- rename(manifest, file_id = id) %>%
+manifest <- as.tibble(manifest) %>%
+  rename(file_id = id) %>%
   rename(manifest_file_id = filename)
-manifest$manifest_file_id <- str_replace(manifest$manifest_file_id,".FPKM-UQ.txt.gz", "")
+manifest$manifest_file_id <- str_replace(manifest$manifest_file_id,".htseq.counts.gz", "")
 corresp_manifestUUID_barcode <- full_join(manifest, corresp_sampleUUID_barcode, by = "file_id") %>%
   dplyr::select(manifest_file_id, submitter_id, bcr_patient_barcode, tissue_type)
 
@@ -118,8 +119,8 @@ tibble_clinannot_all <- dplyr::select(tibble_clinannot_all, -2,-3,-4)           
 
 tibble_expr_sample <- left_join(tibble_hgnc, full_sample_annot, by = "manifest_file_id")
 tibble_expr_sample_clin <- left_join(tibble_expr_sample, tibble_clinannot_all, by = "manifest_file_id")
-tibble_expr_sample_clin <-tibble_expr_sample_clin %>%
-  mutate(patient_id = as.character(tumor_code)) %>%
+tibble_expr_sample_clin <- tibble_expr_sample_clin %>%
+  mutate(tibble_expr_sample_clin,patient_id = as.character(tumor_code)) %>%
   dplyr::filter(!is.na(bcr_patient_barcode)) %>%
   dplyr::filter(!is.na(tumor_code)) %>%
   arrange(tissue_type, bcr_patient_barcode) %>%
@@ -128,4 +129,4 @@ tibble_expr_sample_clin <-tibble_expr_sample_clin %>%
 lookup_table <- c("ACC"="Adrenocortical carcinoma (n=79)","BLCA"="Bladder carcinoma (n=414)","BRCA"="Breast carcinoma (n=1108)","CESC"="Cervical carcinoma (n=306)","CHOL"="Cholangiocarcinoma (n=36)","COAD"="Colon adenocarcinoma (n=477)","DLBC"="B-cell lymphoma (DLBCL) (n=48)","ESCA"="Esophageal carcinoma (n=160)","GBM"="Glioblastoma (n=169)","HNSC"="Head&neck carcinoma (n=502)","KICH"="Kidney chromophobe tumor (n=65)","KIRC"="Kidney renal cell carcinoma (n=539)","KIRP"="Kidney papillary carcinoma (n=289)","LGG"="Low grade glioma (n=1055)","LIHC"="Liver hepatocellular carcinoma (n=368)","LUAD"="Lung adenocarcinoma (n=535)","LUSC"="Lung squamous carcinoma (n=502)","MESO"="Mesothelioma (n=86)","OV"="Ovarian carcinoma (n=379)","PAAD"="Pancreas carcinoma (n=178)","PCPG"="Pheochromocytoma paraganglioma (n=183)","PRAD"="Prostate adenocarcinoma (n=496)","READ"="Rectal adenocarcinoma (n=167)","SARC"="Sarcoma (n=263)","SKCM"="Skin melanoma (n=471)","STAD"="Stomach adenocarcinoma (n=375)","TGCT"="Testicular germ cell tumor (n=156)","THCA"="Thyroid carcinoma (n=509)","THYM"="Thymoma (n=119)","UCEC"="Uterine endometrial carcinoma (n=550)","UCS"="Uterine carcinosarcoma (n=56)","UVM"="Uveal melanoma (n=78)")
 tibble_expr_sample_clin$tumor_type <- lookup_table[tibble_expr_sample_clin$tumor_code]
 
-write.table(tibble_expr_sample_clin, file = "expr_data_select_clin_annot_FPKM-UQ.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(tibble_expr_sample_clin, file = "expr_data_select_clin_annot_counts.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
